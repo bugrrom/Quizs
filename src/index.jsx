748be7f8c00dfa4d28e5521003e6ac3d3c53aaa7
Roman <bugrom@mail.ru>
2020-01-25 13:09:12 +0300

    create_react

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import './style/index.css'
+import React from 'react'
+import {render} from 'react-dom'
+
+render(<div>Hey hey</div>,document.getElementById('root'));
+
+
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const dev = true;
+
+module.exports = {
+    mode: 'development',
+    entry: ['@babel/polyfill', './src/index.jsx'],
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        minimizer: [// минификация css js
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin()
+        ]
+    },
+    devServer: {
+        port: 3200
+    },
+    devtool: dev ? 'source-map' : '',//исходные карты
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+            minify: {
+                collapseWhitespace: true //оптимизация html
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[hash].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|gif|svg|jpg|jpeg)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                },
+                 'eslint-loader'
+                ]
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
